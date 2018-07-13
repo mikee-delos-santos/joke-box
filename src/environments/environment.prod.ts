ae6459460428adcf62abbb5b4eddab3261b1dc09
Mikee <mikeed@sourcepad.com>
2018-07-13 19:13:42 +0800

    Install firebase and configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBRAwoBcj5dyOXQbvbN-yUM3l8ssAdI2W8",
+    authDomain: "jokenas-a5a67.firebaseapp.com",
+    databaseURL: "https://jokenas-a5a67.firebaseio.com",
+    projectId: "jokenas-a5a67",
+    storageBucket: "jokenas-a5a67.appspot.com",
+    messagingSenderId: "619043364819"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBRAwoBcj5dyOXQbvbN-yUM3l8ssAdI2W8",
+    authDomain: "jokenas-a5a67.firebaseapp.com",
+    databaseURL: "https://jokenas-a5a67.firebaseio.com",
+    projectId: "jokenas-a5a67",
+    storageBucket: "jokenas-a5a67.appspot.com",
+    messagingSenderId: "619043364819"
+  }
 };
 
 /*
